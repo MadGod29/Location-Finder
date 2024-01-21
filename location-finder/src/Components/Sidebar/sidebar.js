0f80f61f15cd47ca608988687e646dc7abcbb0a9
Mangeshg29 <mxng29@gmail.com>
2024-01-22 02:10:27 +0530

    modified:   location-finder/src/Components/Sidebar/sidebar.js

--- location-finder/src/Components/Sidebar/sidebar.js
+++ location-finder/src/Components/Sidebar/sidebar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './sidebar.css';
 
 export default function Sidebar() {
+    const shownotif=()=>{
+        alert("Button pressed")
+    }
   return (
   <>
     <div className="mainsidebar" style={{width: '420px', height: '100vh', background: 'white',borderTopRightRadius:'30px',borderBottomRightRadius:'30px',textAlign:'center'}} >
         <div style={{color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '300', wordWrap: 'break-word',padding:'20px'}}>FIND ACCORDING TO </div>
         
         <div style={{display: 'flex', alignItems: 'center'}}>
-            <button style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
+            <button onClick={shownotif} style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
                 <div className="option" style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '290', wordWrap: 'break-word'}}>
                 PEOPLE
                 </div>
@@ -16,7 +19,7 @@ export default function Sidebar() {
 
             <div style={{width: '2px', height: '45px', background: 'black'}}></div>
 
-            <button style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
+            <button onClick={shownotif} style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
                 <div className="option location" style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '290', wordWrap: 'auto'}}>
                 LOCATIONS
                 </div>
@@ -24,12 +27,12 @@ export default function Sidebar() {
         </div>
 
         <div style={{display:'flex',justifyContent: 'space-between',padding:'20px'}}>
-            <button style={{width: '100vh', height: '100%', background: '#3D84EA', borderRadius: 30,marginRight:'20px'}} >
+            <button onClick={shownotif} style={{width: '100vh', height: '100%', background: '#3D84EA', borderRadius: 30,marginRight:'20px'}} >
                 <div style={{color: 'white', fontSize: 20, fontFamily: 'Inter', fontWeight: '300', wordWrap: 'break-word',padding:'10px',paddingTop:'20px',paddingBottom:'20px'}}>
                 ADD PERSON
                 </div>
             </button>
-            <button style={{width: '100vh', height: '100%', background: '#3D84EA', borderRadius: 30}} >
+            <button onClick={shownotif} style={{width: '100vh', height: '100%', background: '#3D84EA', borderRadius: 30}} >
                 <div style={{color: 'white', fontSize: 20, fontFamily: 'Inter', fontWeight: '300', wordWrap: 'break-word',padding:'20px'}}>
                 CALCULATE
                 </div>
